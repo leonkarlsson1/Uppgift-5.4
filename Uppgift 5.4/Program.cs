835207312bc4f60a1a1c55f290163b2b8e4ddcc7
Mopa01 <111962656+Dungeater1337@users.noreply.github.com>
2023-01-27 10:38:53 +0100

    Add project files.

---
+++ Uppgift 5.4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Inlämningsuppgifter_5_4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = { "Adam", "Leon", "Alem", "Tim", "Ameen" };
+            Array.Sort(namn);
+            for (int i = 0; i < namn.Length; i++)
+            {
+                Console.WriteLine(namn[i]);
+            }
+        }
+    }
+}
